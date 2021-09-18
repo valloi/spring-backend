6f0961554834c7545a9471c603864e8db2c41b44
vallo <vallo.isop@gmail.com>
2021-09-18 14:31:17 +0300

    Add /api request mapping

--- src/main/java/org/vi/app/controller/FileController.java
+++ src/main/java/org/vi/app/controller/FileController.java
@@ -5,6 +5,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.vi.app.service.FileService;
 
@@ -12,6 +13,7 @@ import org.vi.app.service.FileService;
 @RestController
 @AllArgsConstructor
 @Log4j2
+@RequestMapping("/api")
 public class FileController {
 
     FileService fileService;
