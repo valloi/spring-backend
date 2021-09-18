83b679fbb159029b9ae99e92d1f20e2bfbec93bb
vallo <vallo.isop@gmail.com>
2021-09-18 14:31:32 +0300

    Clean up FileService

--- src/main/java/org/vi/app/service/FileService.java
+++ src/main/java/org/vi/app/service/FileService.java
@@ -4,13 +4,9 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.util.Base64;
 
 @Service
 @Log4j2
