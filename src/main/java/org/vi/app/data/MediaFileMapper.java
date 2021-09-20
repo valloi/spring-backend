c0317d1430bd0a2ccf6dca71b8d1ddb5258e9afa
vallo <vallo.isop@gmail.com>
2021-09-21 01:52:11 +0300

    Remove ConversionUtil class.

--- src/main/java/org/vi/app/data/MediaFileMapper.java
+++ src/main/java/org/vi/app/data/MediaFileMapper.java
@@ -2,15 +2,22 @@ package org.vi.app.data;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.vi.app.data.util.ConversionUtil;
+import org.mapstruct.Named;
 
-@Mapper(componentModel = "spring", uses = ConversionUtil.class)
+import java.util.Base64;
+
+@Mapper(componentModel = "spring")
 public interface MediaFileMapper {
     MediaFileDto fromEntity(MediaFile entity);
 
     MediaFile fromDto(MediaFileDto dto);
 
-    @Mapping(target = "contentsBase64", source = "contents", qualifiedBy = ConversionUtil.ToBase64.class)
+    @Mapping(target = "contentsBase64", source = "contents", qualifiedByName = "toBase64")
     MediaFileBase64 toBase64(MediaFile entity);
+
+    @Named("toBase64")
+    default String bytesToBase64(byte[] in) {
+        return in.length == 0 ? null : Base64.getEncoder().encodeToString(in);
+    }
 
 }
