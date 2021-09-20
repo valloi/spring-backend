80820c0c37875fc1115252a44f7536554ac99bbc
vallo <vallo.isop@gmail.com>
2021-09-21 00:46:37 +0300

    Remove unused annotations from ConversionUtil

--- src/main/java/org/vi/app/data/util/ConversionUtil.java
+++ src/main/java/org/vi/app/data/util/ConversionUtil.java
@@ -2,18 +2,11 @@ package org.vi.app.data.util;
 
 import org.mapstruct.Qualifier;
 import org.springframework.stereotype.Component;
-
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
 import java.util.Base64;
 
 @Component
 public class ConversionUtil {
     @Qualifier
-    @Target(ElementType.METHOD)
-    @Retention(RetentionPolicy.SOURCE)
     public @interface ToBase64 { }
 
     @ToBase64
