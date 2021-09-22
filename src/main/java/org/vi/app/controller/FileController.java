44020c2b1f3c85b0ee2bdb870705772b3dd6dccb
vallo <vallo.isop@gmail.com>
2021-09-22 20:59:40 +0300

    Added /gettwofiles endpoint

--- src/main/java/org/vi/app/controller/FileController.java
+++ src/main/java/org/vi/app/controller/FileController.java
@@ -7,8 +7,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.vi.app.data.MediaFile;
 import org.vi.app.service.FileService;
 
+import static org.vi.app.util.FileUtils.zipMediaFiles;
 
 @RestController
 @AllArgsConstructor
@@ -26,6 +28,19 @@ public class FileController {
                         MediaType.parseMediaType("application/pdf"))
                 .header("Content-Disposition", "attachment;filename=\"Whiskey_in_the_jar.pdf\"")
                 .body(fileService.getWhiskeyFile());
+    }
+
+    @GetMapping("/gettwofiles")
+    public ResponseEntity<byte[]> getTwoFiles() {
+        MediaFile whiskeyFile = fileService.getFileByName("Whiskey_in_the_jar.pdf");
+        MediaFile orcFile = fileService.getFileByName("Overall ORC I.pdf");
+
+        return ResponseEntity
+                .ok()
+                .contentType(
+                        MediaType.parseMediaType("application/zip"))
+                .header("Content-Disposition", "attachment;filename=\"documents.zip\"")
+                .body(zipMediaFiles(whiskeyFile, orcFile));
     }
 
 }
